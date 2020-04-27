21f3741d0197a678002512f2f65fa233b865a77d
aimprohub <46415898+aimprohub@users.noreply.github.com>
2020-04-27 20:27:20 +0530

    space added

--- blog/templates/blog/firstjs.js
+++ blog/templates/blog/firstjs.js
@@ -8,5 +8,6 @@ var fileInput = document.getElementById("csv"),
         // start reading the file. When it is done, calls the onload event defined above.
         reader.readAsBinaryString(fileInput.files[0]);
     };
+    
 
 fileInput.addEventListener('change', readFile);
